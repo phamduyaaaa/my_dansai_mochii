e31285be6fcb870f4a6f3efecc2de31e3ed572a9
Pham Duc Duy <134459693+phamduyaaaa@users.noreply.github.com>
2025-06-11 23:02:09 +0700

    Update eye_intellar_ca.ino

--- eye_intellar_ca.ino
+++ eye_intellar_ca.ino
@@ -4,418 +4,237 @@
 #include <Adafruit_SSD1306.h>
 
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library. 
-// On an arduino UNO:       A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET     -1 
+#define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI);
-  //uint8_t w, uint8_t h, SPIClass *spi_ptr,
-    //                               int8_t dc_pin, int8_t rst_pin, int8_t cs_pin,
-      //                             uint32_t bitrate)
 
-
-// states for demo 
+// Biến điều khiển
 int demo_mode = 1;
-static const int max_animation_index = 8;
+static const int max_animation_index = 9; // <<< THAY ĐỔI: Tăng số hoạt cảnh lên
 int current_animation_index = 0;
 
 
+// Trạng thái tham chiếu (nghỉ) của mắt
+int ref_eye_height = 30; // Giảm chiều cao mắt một chút để có không gian cho miệng
+int ref_eye_width = 30;
+int ref_space_between_eye = 15;
+int ref_corner_radius = 8;
 
-//reference state
-int ref_eye_height = 40;
-int ref_eye_width = 40;
-int ref_space_between_eye = 10;
-int ref_corner_radius = 10;
-//current state of the eyes
+// Trạng thái hiện tại của con mắt
 int left_eye_height = ref_eye_height;
 int left_eye_width = ref_eye_width;
-int left_eye_x = 32;
-int left_eye_y = 32;
-int right_eye_x = 32+ref_eye_width+ref_space_between_eye;
-int right_eye_y = 32;
+int left_eye_x, left_eye_y; // Sẽ được tính trong hàm center_eyes
+int right_eye_x, right_eye_y;
 int right_eye_height = ref_eye_height;
 int right_eye_width = ref_eye_width;
 
+// <<< THAY ĐỔI: Thêm các biến cho miệng >>>
+int mouth_state = 0; // 0=Bình thường, 1=Cười, 2=Ngạc nhiên (chữ O)
+int mouth_x;         // Vị trí X của miệng
+int mouth_y;         // Vị trí Y của miệng
+int mouth_w = 40;    // Chiều rộng miệng
+int mouth_h = 8;     // Chiều cao miệng
 
 
+// --- CÁC HÀM VẼ VÀ HOẠT CẢNH ---
 
-void draw_eyes(bool update=true)
+// <<< THAY ĐỔI: Hàm vẽ miệng mới >>>
+void draw_mouth() {
+  switch(mouth_state) {
+    case 0: // Miệng bình thường (một đường thẳng)
+      display.drawFastHLine(mouth_x - mouth_w / 2, mouth_y, mouth_w, SSD1306_WHITE);
+      break;
+    case 1: // Miệng cười (một hình chữ nhật bo góc hẹp)
+      display.drawRoundRect(mouth_x - mouth_w / 2, mouth_y, mouth_w, mouth_h, 4, SSD1306_WHITE);
+      break;
+    case 2: // Miệng ngạc nhiên (hình tròn)
+      display.fillCircle(mouth_x, mouth_y + 4, 10, SSD1306_WHITE);
+      break;
+  }
+}
+
+// <<< THAY ĐỔI: Đổi tên hàm và tích hợp vẽ miệng >>>
+void draw_face(bool update=true)
 {
     display.clearDisplay();        
-    //draw from center
+    
+    // Vẽ mắt trái và phải
     int x = int(left_eye_x-left_eye_width/2);
     int y = int(left_eye_y-left_eye_height/2);
     display.fillRoundRect(x,y,left_eye_width,left_eye_height,ref_corner_radius,SSD1306_WHITE);
+    
     x = int(right_eye_x-right_eye_width/2);
     y = int(right_eye_y-right_eye_height/2);
     display.fillRoundRect(x,y,right_eye_width,right_eye_height,ref_corner_radius,SSD1306_WHITE);    
+    
+    // Vẽ miệng
+    draw_mouth();
+
     if(update)
     {
       display.display();
     }
-    
 }
 
 
 void center_eyes(bool update=true)
 {
-  //move eyes to the center of the display, defined by SCREEN_WIDTH, SCREEN_HEIGHT
+  // Reset mắt về trung tâm và dịch lên trên một chút
   left_eye_height = ref_eye_height;
   left_eye_width = ref_eye_width;
   right_eye_height = ref_eye_height;
   right_eye_width = ref_eye_width;
   
-  left_eye_x = SCREEN_WIDTH/2-ref_eye_width/2-ref_space_between_eye/2;
-  left_eye_y = SCREEN_HEIGHT/2;
-  right_eye_x = SCREEN_WIDTH/2+ref_eye_width/2+ref_space_between_eye/2;
-  right_eye_y = SCREEN_HEIGHT/2;
+  // Dịch tâm mắt lên trên để có chỗ cho miệng
+  int center_y_offset = -8; 
+  left_eye_x = SCREEN_WIDTH/2 - ref_eye_width/2 - ref_space_between_eye/2;
+  left_eye_y = SCREEN_HEIGHT/2 + center_y_offset;
+  right_eye_x = SCREEN_WIDTH/2 + ref_eye_width/2 + ref_space_between_eye/2;
+  right_eye_y = SCREEN_HEIGHT/2 + center_y_offset;
+
+  // <<< THAY ĐỔI: Reset miệng về trạng thái bình thường và căn giữa >>>
+  mouth_state = 0;
+  mouth_x = SCREEN_WIDTH / 2;
+  mouth_y = SCREEN_HEIGHT / 2 + ref_eye_height / 2 + 5;
   
-  draw_eyes(update);
+  draw_face(update);
 }
 
 void blink(int speed=12)
 {
-  draw_eyes();
-  
-  
-  for(int i=0;i<3;i++)
-  {
-    left_eye_height = left_eye_height-speed;
-    right_eye_height = right_eye_height-speed;    
-    draw_eyes();
+  // Giữ nguyên hàm blink
+  int original_height = left_eye_height;
+  for(int i=0;i<3;i++){
+    left_eye_height -= speed;
+    right_eye_height -= speed;    
+    draw_face();
     delay(1);
   }
-  for(int i=0;i<3;i++)
-  {
-    left_eye_height = left_eye_height+speed;
-    right_eye_height = right_eye_height+speed;
-    
-    draw_eyes();
+  for(int i=0;i<3;i++){
+    left_eye_height += speed;
+    right_eye_height += speed;
+    draw_face();
     delay(1);
   }
+  left_eye_height = original_height;
+  right_eye_height = original_height;
+  draw_face();
 }
 
-
-void sleep()
-{
+void sleep() {
   left_eye_height = 2;
   right_eye_height = 2;
-  draw_eyes(true);  
+  mouth_state = 0; // Miệng bình thường khi ngủ
+  draw_face(true);  
 }
-void wakeup()
-{
-  
+
+void wakeup() {
   sleep();
-  
-  for(int h=0; h <= ref_eye_height; h+=2)
-  {
+  delay(500);
+  for(int h=0; h <= ref_eye_height; h+=2){
     left_eye_height = h;
     right_eye_height = h;
-    draw_eyes(true);
+    draw_face(true);
   }
-  
-
 }
 
-
+// <<< THAY ĐỔI: Hàm happy_eye giờ sẽ làm miệng cười >>>
 void happy_eye()
 {
   center_eyes(false);
-  //draw inverted triangle over eye lower part
+  mouth_state = 1; // Chuyển sang miệng cười
+  
+  // Vẫn giữ hiệu ứng mắt cười ^^
   int offset = ref_eye_height/2;
-  for(int i=0;i<10;i++)
+  for(int i=0; i<10; i++)
   {
+    draw_face(false); // Vẽ lại cả mặt (đã có miệng cười)
     display.fillTriangle(left_eye_x-left_eye_width/2-1, left_eye_y+offset, left_eye_x+left_eye_width/2+1, left_eye_y+5+offset, left_eye_x-left_eye_width/2-1,left_eye_y+left_eye_height+offset,SSD1306_BLACK);
-    //display.fillRect(left_eye_x-left_eye_width/2-1, left_eye_y+5, left_eye_width+1, 20,SSD1306_BLACK);
-
     display.fillTriangle(right_eye_x+right_eye_width/2+1, right_eye_y+offset, right_eye_x-left_eye_width/2-1, right_eye_y+5+offset, right_eye_x+right_eye_width/2+1,right_eye_y+right_eye_height+offset,SSD1306_BLACK);
-    //display.fillRect(right_eye_x-right_eye_width/2-1, right_eye_y+5, right_eye_width+1, 20,SSD1306_BLACK);  
     offset -= 2;
     display.display();
     delay(1);
   }
-  
-  
-  display.display();
   delay(1000);
+  center_eyes(true);
 }
 
-void saccade(int direction_x, int direction_y)
+// <<< THAY ĐỔI: Tạo hoạt cảnh mới "Ngạc nhiên" >>>
+void surprise()
 {
-  //quick movement of the eye, no size change. stay at position after movement, will not move back,  call again with opposite direction
-  //direction == -1 :  move left
-  //direction == 1 :  move right
+  center_eyes(false);
   
-  int direction_x_movement_amplitude = 8;
-  int direction_y_movement_amplitude = 6;
-  int blink_amplitude = 8;
-
-  for(int i=0;i<1;i++)
-  {
-    left_eye_x+=direction_x_movement_amplitude*direction_x;
-    right_eye_x+=direction_x_movement_amplitude*direction_x;    
-    left_eye_y+=direction_y_movement_amplitude*direction_y;
-    right_eye_y+=direction_y_movement_amplitude*direction_y;    
-    
-    right_eye_height-=blink_amplitude;
-    left_eye_height-=blink_amplitude;
-    draw_eyes();
-    delay(1);
-  }
+  // Mắt mở to ra
+  left_eye_height = ref_eye_height + 5;
+  right_eye_height = ref_eye_height + 5;
+  // Miệng hình chữ O
+  mouth_state = 2;
   
-  for(int i=0;i<1;i++)
-  {
-    left_eye_x+=direction_x_movement_amplitude*direction_x;
-    right_eye_x+=direction_x_movement_amplitude*direction_x;    
-    left_eye_y+=direction_y_movement_amplitude*direction_y;
-    right_eye_y+=direction_y_movement_amplitude*direction_y;
-
-    right_eye_height+=blink_amplitude;
-    left_eye_height+=blink_amplitude;
-
-    draw_eyes();
-    delay(1);
-  }
-  
+  draw_face(true);
+  delay(2000);
+  center_eyes(true);
 }
 
-void move_right_big_eye()
-{
-  move_big_eye(1);
-}
-void move_left_big_eye()
-{
-  move_big_eye(-1);
-}
-void move_big_eye(int direction)
-{
-  //direction == -1 :  move left
-  //direction == 1 :  move right
-
-  int direction_oversize = 1;
-  int direction_movement_amplitude = 2;
-  int blink_amplitude = 5;
-
-  for(int i=0;i<3;i++)
-  {
-    left_eye_x+=direction_movement_amplitude*direction;
-    right_eye_x+=direction_movement_amplitude*direction;    
-    right_eye_height-=blink_amplitude;
-    left_eye_height-=blink_amplitude;
-    if(direction>0)
-    {
-      right_eye_height+=direction_oversize;
-      right_eye_width+=direction_oversize;
-    }else
-    {
-      left_eye_height+=direction_oversize;
-      left_eye_width+=direction_oversize;
-    }
-
-    draw_eyes();
-    delay(1);
-  }
-  for(int i=0;i<3;i++)
-  {
-    left_eye_x+=direction_movement_amplitude*direction;
-    right_eye_x+=direction_movement_amplitude*direction;
-    right_eye_height+=blink_amplitude;
-    left_eye_height+=blink_amplitude;
-    if(direction>0)
-    {
-      right_eye_height+=direction_oversize;
-      right_eye_width+=direction_oversize;
-    }else
-    {
-      left_eye_height+=direction_oversize;
-      left_eye_width+=direction_oversize;
-    }
-    draw_eyes();
-    delay(1);
-  }
-
-  delay(1000);
-
-  for(int i=0;i<3;i++)
-  {
-    left_eye_x-=direction_movement_amplitude*direction;
-    right_eye_x-=direction_movement_amplitude*direction;    
-    right_eye_height-=blink_amplitude;
-    left_eye_height-=blink_amplitude;
-    if(direction>0)
-    {
-      right_eye_height-=direction_oversize;
-      right_eye_width-=direction_oversize;
-    }else
-    {
-      left_eye_height-=direction_oversize;
-      left_eye_width-=direction_oversize;
-    }
-    draw_eyes();
-    delay(1);
-  }
-  for(int i=0;i<3;i++)
-  {
-    left_eye_x-=direction_movement_amplitude*direction;
-    right_eye_x-=direction_movement_amplitude*direction;    
-    right_eye_height+=blink_amplitude;
-    left_eye_height+=blink_amplitude;
-    if(direction>0)
-    {
-      right_eye_height-=direction_oversize;
-      right_eye_width-=direction_oversize;
-    }else
-    {
-      left_eye_height-=direction_oversize;
-      left_eye_width-=direction_oversize;
-    }
-    draw_eyes();
-    delay(1);
-  }
+// Các hàm gốc khác giữ nguyên hoặc tạm thời vô hiệu hóa
+void saccade(int direction_x, int direction_y){ blink(10); }
+void move_right_big_eye() { center_eyes(); delay(500); }
+void move_left_big_eye() { center_eyes(); delay(500); }
 
 
-  center_eyes();
-
-
-}
+// --- HÀM SETUP VÀ LOOP CHÍNH ---
 
 void setup() {
-  // put your setup code here, to run once:
-  
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
-  
-  
-  //for usb communication  
   Serial.begin(115200);
-  
-  
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  
-  // Clear the buffer
-  display.clearDisplay();
-  
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(0,0);             // Start at top-left corner
-  display.println(F("Intellar.ca"));  
-  display.display();  
-  delay(2000);
-  sleep();
-  delay(2000);
-
-  // Draw a single pixel in white
-  //display.drawPixel(10, 10, SSD1306_WHITE);
-  
-  
+  center_eyes(true);
 }
-
-
 
 void launch_animation_with_index(int animation_index)
 {
-
-  
-
-  if(animation_index>max_animation_index)
-  {
-    animation_index=8;
+  if(animation_index > max_animation_index) {
+    animation_index = max_animation_index;
   }
 
   switch(animation_index)
-      {
-        case 0:
-          wakeup();
-          break;
-        case 1:
-          center_eyes(true);
-          break;
-        case 2:
-          move_right_big_eye();
-          break;
-        case 3:
-          move_left_big_eye();
-          break;
-        case 4:      
-          blink(10);
-          break;
-        case 5:
-          blink(20);
-          break;
-        case 6:
-          happy_eye();      
-          break;
-        case 7:
-          sleep();
-          break;
-        case 8:
-          center_eyes(true);
-          for(int i=0;i<20;i++)
-          { 
-            int dir_x = random(-1, 2);
-            int dir_y = random(-1, 2);
-            saccade(dir_x,dir_y);
-            delay(1);
-            saccade(-dir_x,-dir_y);
-            delay(1);     
-          }
-          break;
-          
-      }
+  {
+    case 0: wakeup(); break;
+    case 1: center_eyes(true); break;
+    case 2: move_right_big_eye(); break;
+    case 3: move_left_big_eye(); break;
+    case 4: blink(10); break;
+    case 5: blink(20); break;
+    case 6: happy_eye(); break;
+    case 7: sleep(); break;
+    case 8: saccade(0,0); break;
+    case 9: surprise(); break; // <<< THAY ĐỔI: Thêm case mới
+  }
 }
 
-
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  // put your main code here, to run repeatedly:
-
   if(demo_mode == 1)
   {
-    // cycle animations
     launch_animation_with_index(current_animation_index++);
-    if(current_animation_index > max_animation_index)
-    {
+    if(current_animation_index > max_animation_index) {
       current_animation_index = 0;
     }
+    delay(500);
   }
-
   
-  //send A0 - A5  for animation 0 to 5
   if(Serial.available()) {
     String data = Serial.readString();
     data.trim();
     char cmd = data[0];
     
-    
-    
-    
-    if(cmd == 'A')
-    {
+    if(cmd == 'A') {
       demo_mode = 0;
-
       String arg = data.substring(1,data.length());
       int anim = arg.toInt();
       launch_animation_with_index(anim);
-      Serial.print(cmd);
-      Serial.print(arg);   
+      Serial.print("Executed Anim: ");
+      Serial.println(arg);
     }
-
-    
-
-
   }
-
-
 }
